3ce6b1a13e97aa91477534a927da6ed6f27486e5
ST10118368 <142236222+ST10118368@users.noreply.github.com>
2024-09-17 16:59:03 +0200

    Add files via upload
    The latest updated one

---
+++ ST10118368_Ledwaba_David_Group1_PROG7312_Part1_Latest/CityCharm/CityCharm/ServicesRequestForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CityCharm
+{
+    public partial class ServicesRequestForm : Form
+    {
+        public ServicesRequestForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
